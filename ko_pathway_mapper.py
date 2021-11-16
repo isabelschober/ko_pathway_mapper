d9c0923d005f96d77861f79a271773a74dc1235e
Isabel Schober <47421170+isabelschober@users.noreply.github.com>
2021-11-16 14:09:46 +0100

    Update and rename blastkoala_pathway_mapper.py to ko_pathway_mapper.py

--- ko_pathway_mapper.py
+++ ko_pathway_mapper.py
@@ -7,8 +7,7 @@ import argparse
 Find corresponding pathways to list of KOs from BlastKOALA results
 
 Usage:
-
-bloastkoala_pathway_mapper.py -k <BlastKOALA results> -d <KEGG database file> (-o <output file>)
+ko_pathway_mapper.py -k <BlastKOALA results> -d <KEGG database file> (-o <output file>)
 
 KEGG database file from https://www.genome.jp/kegg-bin/get_htext?ko00001.keg 
 Click "Download htext"
